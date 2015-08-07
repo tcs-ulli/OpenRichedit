13935b8f56c063e7a278abb3759e998fcb376e68
Chris <ulrich@tcs-germany.de>
2015-08-07 15:04:42 +0200

    -

--- domlayout.pas
+++ domlayout.pas
@@ -5,60 +5,112 @@ unit DOMLayout;
 interface
 
 uses
-  Classes, SysUtils, DOM;
+  Classes, SysUtils, DOM, DOM_HTML, FPCanvas;
 
 type
+  TLayoutNode = class;
+  TLayoutNodeClass = class of TLayoutNode;
   TLayoutedDocument = class
+  private
+    FLayout: TLayoutNode;
+    FCanvas : TFPCustomCanvas;
   public
     procedure ReadFromDOM(aDoc : TDOMDocument);
+    property Layout : TLayoutNode read FLayout;
+    procedure Clear;
   end;
 
-  TLayoutNode = class
+  TLayoutNode = class(TList)
+  private
+    FNode: TDOMNode;
+    FParent: TLayoutedDocument;
+    function GetChilds(Index : Integer): TLayoutNode;
   public
-    constructor Create(aDomNode : TDOMNode);
+    constructor Create(aDoc : TLayoutedDocument; aDomNode : TDOMNode);
     procedure Calculate;virtual;abstract;
+    function FindNodeType(aNode : TDOMNode) : TLayoutNodeClass;
     property DOMNode : TDOMNode read FNode;
+    property Parent : TLayoutedDocument read FParent;
+    property Childs[Index : Integer] : TLayoutNode read GetChilds;
   end;
 
-  TInvisibleNode = class(TLayoutNode)
+  TLayoutInvisibleNode = class(TLayoutNode)
+  public
+    procedure Calculate;override;
+  end;
+
+  TLayoutFont = class(TLayoutInvisibleNode)
+  public
+    procedure Calculate;override;
+  end;
+
+  TLayoutText = class(TLayoutNode)
   public
     procedure Calculate;override;
   end;
 
 implementation
 
-procedure TInvisibleNode.Calculate;
+procedure TLayoutText.Calculate;
+begin
+
+  DOMNode.TextContent;
+end;
+
+procedure TLayoutInvisibleNode.Calculate;
 begin
 end;
 
-procedure TLayoutedDocument.ReadFromDOM(aDoc: TDOMDocument);
-  procedure ProcessNode(Node: TDOMNode);
-  var
-    cNode: TDOMNode;
-  begin
-    if Node = nil then Exit; // Stops if reached a leaf
+procedure TLayoutFont.Calculate;
+begin
+  inherited Calculate;
+  //Apply font Settings
+end;
 
+function TLayoutNode.GetChilds(Index : Integer): TLayoutNode;
+begin
+  Result := TLayoutNode(Items[Index]);
+end;
 
-
-    // Goes to the child node
-    cNode := Node.FirstChild;
-    // Processes all child nodes
-    while cNode <> nil do
+constructor TLayoutNode.Create(aDoc: TLayoutedDocument; aDomNode: TDOMNode);
+var
+  cNode: TDOMNode;
+begin
+  inherited Create;
+  FNode := aDomNode;
+  FParent := aDoc;
+  cNode := aDomNode.FirstChild;
+  while Assigned(cNode) do
     begin
-      ProcessNode(cNode);
+      Add(FindNodeType(cNode).Create(FParent,cNode));
       cNode := cNode.NextSibling;
     end;
-  end;
+end;
 
+function TLayoutNode.FindNodeType(aNode: TDOMNode): TLayoutNodeClass;
+begin
+  Result := TLayoutInvisibleNode;
+  if (aNode is THTMLFontElement) or (aNode is THTMLBaseFontElement) then
+    Result := TLayoutFont
+  else if (aNode is TDOMText) then
+    Result := TLayoutText
+  else
+    begin
+      writeln(aNode.ClassName)
+    end;
+end;
+
+procedure TLayoutedDocument.ReadFromDOM(aDoc: TDOMDocument);
 var
   iNode: TDOMNode;
 begin
-  iNode := aDoc.DocumentElement.FirstChild;
-  while iNode <> nil do
-    begin
-      ProcessNode(iNode);
-      iNode := iNode.NextSibling;
-    end;
+  FLayout.Free;
+  FLayout := TLayoutNode.Create(Self,aDoc.DocumentElement);
+end;
+
+procedure TLayoutedDocument.Clear;
+begin
+  FreeAndNil(FLayout);
 end;
 
 end.
